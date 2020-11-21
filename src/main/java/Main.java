ce18b3c76bcdb361606cbdb7482d2e54f7686f41
Rohin Joseph <rohinj99@gmail.com>
2020-11-21 20:30:00 +0000

    Added Shippable YML

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import org.joda.time.DateTime;
+import java.util.ArrayList;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+
+public class Main {
+
+
+    public static void main(String [] args){
+
+        ArrayList<Student> students = new ArrayList<Student>(100);
+        ArrayList<Module> modules = new ArrayList<Module>(100);
+        ArrayList<Course> courses = new ArrayList<Course>(100);
+
+
+    }
+
+
+
+}
